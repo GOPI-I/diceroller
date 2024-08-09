91b46329b4ec8788f8ce1d6af6c83d198f02d800
GOPI-I <igopi170@gmail.com>
2024-08-09 15:02:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from "./home";
+import About from "./about";
+import { createBrowserRouter,RouterProvider} from "react-router-dom";
+
+const config = createBrowserRouter([
+  {path:"/home",element:<Home/>},
+  {path:"/about",element:<About/>}
+]);
 
 function App() {
+  const getUser = async ()=>{
+    let res = await fetch("https://reqres.in/api/users");
+    let server = await res.json();
+    console.log(server['data']);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <RouterProvider config={config}/> */}
+      <button onClick={getUser}> Mens are Brave</button>
+      <About/>
+      <Home/>
     </div>
+   
   );
 }
 
---
+++ src/about.js
@@ -0,0 +1,4 @@
+function About(){
+    return <h1>This is about about page</h1>
+}
+export default About;
